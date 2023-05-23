c58f999fe499795ee0e266ec4201c94f9119e981
Dewi Nahrisa Amelia <dewinahrisaamelia@Dewis-MacBook-Air.local>
2023-05-23 19:19:29 +0700

    first commit

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -32,8 +32,9 @@
     </nav>
 
     <div class="container-sm mt-5">
-        <form action="{{ route('employees.store') }}" method="POST">
+        <form action="{{ route('employees.update',['employee' => $employee->employee_id]) }}" method="POST">
             @csrf
+            @method('PUT')
             <div class="row justify-content-center">
                 <div class="p-5 bg-light rounded-3 border col-xl-6">
 
@@ -48,49 +49,51 @@
 
                     <div class="mb-3 text-center">
                         <i class="bi-person-circle fs-1"></i>
-                        <h4>Create Employee</h4>
+                        <h4>Edit Employee</h4>
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName" class="form-label">First Name</label>
-                            <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="" placeholder="Enter First Name">
+                            <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{$employee -> firstname }}" placeholder="Enter First Name">
                             @error('firstName')
-                            <div class="invalid-feedback">{{$message}}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="lastName" class="form-label">Last Name</label>
-                            <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="" placeholder="Enter Last Name">
+                            <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ $employee -> lastname }}" placeholder="Enter Last Name">
                             @error('lastName')
-                            <div class="invalid-feedback">{{$message}}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="" placeholder="Enter Email">
+                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ $employee -> email }}" placeholder="Enter Email">
                             @error('email')
-                            <div class="invalid-feedback">{{$message}}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="age" class="form-label">Age</label>
-                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="" placeholder="Enter Age">
+                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ $employee -> age }}" placeholder="Enter Age">
                             @error('age')
-                            <div class="invalid-feedback">{{$message}}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label for="position" class="form-label">Position</label>
-                        <select name="position" id="position" class="form-select">
-                            @foreach ($positions as $position)
-                                <option value="{{ $position->id }}" {{ old('position') == $position->id ? 'selected' : '' }}>{{ $position->code.' - '.$position->name }}</option>
-                            @endforeach
-                        </select>
-                        @error('position')
-                            <div class="text-danger"><small>{{ $message }}</small></div>
-                        @enderror
+                        <div class="col-md-12 mb-3">
+                            <label for="position" class="form-label">Position</label>
+                            <select name="position" id="position" class="form-select">
+                                @foreach ($positions as $position)
+                                    <option value="{{ $position->id }}" {{ old('position') == $position->id ? 'selected' : '' }}>{{ $position->code.' - '.$position->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('position')
+                                <div class="text-danger"><small>{{ $message }}</small></div>
+                            @enderror
+                        </div>
+
+
                     </div>
                     <hr>
                     <div class="row">
